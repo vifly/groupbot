aaa9029cdac00d2617c394dbfb2cddc010157b42
Vifly <viflythink@gmail.com>
2020-10-01 12:54:32 +0800

    add peewee

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utils import *
 
 
 try:
-    from conf import TOKEN, LOGDATEFMT
+    from conf import TOKEN, LOGDATEFMT, DBPATH
 except Exception as e:
     logging.error(e)
     exit(-1)
@@ -21,11 +21,15 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(func=lambda message: True)
 def check_url(message):
     urls = get_urls(message.text)
+    group_name = message.chat.title
+    message_id = message.message_id
+
+    db = BotDatabase(DBPATH)
 
     # For test
     urls = "".join(urls)
     if urls != "":
-        bot.reply_to(message, urls)
+        bot.reply_to(message, f"提取的链接：{urls} 群组名称：{group_name} 消息 ID：{message_id}")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 from urlextract import URLExtract
+from peewee import *
+
+DB = SqliteDatabase(None)
 
 
 def get_urls(message: str) -> list:
@@ -7,9 +10,31 @@ def get_urls(message: str) -> list:
     return urls
 
 
-def add_to_db(url: str, db_path: str):
-    pass
+def get_tg_message_url(chat_name: str, message_id: str) -> str:
+    raise NotImplementedError
 
 
-def is_in_db(url: str) -> bool:
-    return False
+class BotDatabase:
+    class Message(Model):
+        url = TextField()
+        group_name = TextField()
+        tg_message_url = TextField()
+
+        class Meta:
+            database = DB
+
+    def __init__(self, db_path: str):
+        DB.init(db_path)
+        with DB:
+            DB.create_tables([self.Message])
+
+    def add(self, url: str, group_name: str, tg_message_url: str = ""):
+        message = self.Message(url=url, group_name=group_name, tg_message_url=tg_message_url)
+        message.save()
+
+    def is_url_in_db(self, url: str) -> bool:
+        try:
+            result = self.Message.get(self.Message.url == url)
+            return True
+        except:
+            return False
